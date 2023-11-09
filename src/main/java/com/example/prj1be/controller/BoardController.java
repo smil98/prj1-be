4c2771073160f5d205f3a0e3cb51f3a0900df12a
smil98 <seungmilee98@gmail.com>
2023-11-09 14:17:47 +0900

    Added validate function for BoardWrite where it checks and returns badRequest if any of the input is null or blank

--- src/main/java/com/example/prj1be/controller/BoardController.java
+++ src/main/java/com/example/prj1be/controller/BoardController.java
@@ -1,7 +1,11 @@
 package com.example.prj1be.controller;
 
+import com.example.prj1be.domain.Board;
+import com.example.prj1be.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class BoardController {
 
 
+    private final BoardService service;
+
     @PostMapping("add")
-    public void add() {
-        System.out.println("BoardController.add");
+    public ResponseEntity add(@RequestBody Board board) {
+        if(!service.validate(board)) {
+            return ResponseEntity.badRequest().build();
+        }
+        System.out.println("board = " + board);
+        if(service.save(board)) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
