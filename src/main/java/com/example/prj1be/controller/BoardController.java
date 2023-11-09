b91af802f8dc5ec68fbbd130d1dce3a396e011e7
smil98 <seungmilee98@gmail.com>
2023-11-09 15:05:39 +0900

    Minor fixes

--- src/main/java/com/example/prj1be/controller/BoardController.java
+++ src/main/java/com/example/prj1be/controller/BoardController.java
@@ -4,16 +4,12 @@ import com.example.prj1be.domain.Board;
 import com.example.prj1be.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/board")
 public class BoardController {
-
 
     private final BoardService service;
 
@@ -29,4 +25,5 @@ public class BoardController {
             return ResponseEntity.internalServerError().build();
         }
     }
+
 }
