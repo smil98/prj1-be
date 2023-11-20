6c9a6ecd0fa734310700a79ac4a573fd5373eda1
smil98 <seungmilee98@gmail.com>
2023-11-20 14:48:29 +0900

    s3 setting

--- src/main/java/com/example/prj1be/service/BoardService.java
+++ src/main/java/com/example/prj1be/service/BoardService.java
@@ -7,9 +7,14 @@ import com.example.prj1be.mapper.CommentMapper;
 import com.example.prj1be.mapper.FileMapper;
 import com.example.prj1be.mapper.LikeMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
+import software.amazon.awssdk.services.s3.model.PutObjectAclRequest;
 
 import java.io.File;
 import java.io.IOException;
@@ -26,6 +31,13 @@ public class BoardService {
     private final CommentMapper commentMapper;
     private final LikeMapper likeMapper;
     private final FileMapper fileMapper;
+
+    private final S3Client s3;
+
+    @Value("${aws.s3.bucket.name}")
+    private final String bucket;
+
+
 
     public boolean save(Board board, MultipartFile[] files, Member login) throws IOException {
         board.setWriter(login.getId());
@@ -44,13 +56,15 @@ public class BoardService {
     private void upload(Integer boardId, MultipartFile file) throws IOException {
         // C:\Temp\prj1\boardId\fileName
 
-            File folder = new File("C:\\Temp\\prj1\\" + boardId);
-            if(!folder.exists()) {
-                folder.mkdirs();
-            }
-            String path = folder.getAbsolutePath() +  "\\" + file.getOriginalFilename();
-            File des = new File(path);
-            file.transferTo(des);
+        String key = "prj1" + boardId + "/" + file.getOriginalFilename();
+
+        PutObjectAclRequest objectRequest = PutObjectAclRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .acl(ObjectCannedACL.PUBLIC_READ)
+                .build();
+
+        s3.put(objectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
     }
 
     public boolean validate(Board board) {
